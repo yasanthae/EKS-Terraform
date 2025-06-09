92b4b2b32162b2bd2d1d53503af099a7c3a39b0b
yasanthae <yasanthadilshan3600@gmail.com>
2025-06-09 11:45:05 +0530

    adding files

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">=0.12.0"
+  backend "s3" {
+    region  = "ap-south-1"
+    profile = "default"
+    key     = "terraformstatefile"
+    bucket  = "eks-terra-s3"
+  }
+}
---
+++ modules/vpc/main.tf
@@ -0,0 +1,45 @@
+################################################################################
+# VPC Module
+################################################################################
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  providers = {
+    aws = aws.ap-south-1
+  }
+
+  azs = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  #private_subnets     = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  #public_subnets      = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Product = "micro"
+  }
+}
+
+
+################################################################################
+# VPC Endpoints Module
+################################################################################
+
+# Implement this later
